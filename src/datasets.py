fb07e67a12da33dfe1e7e2f432d985ee14239a4f
Gary Chung <kuan-i.chung@vanderbilt.edu>
2023-11-27 13:37:21 -0600

    feat: paired datasets and trainer ready

--- src/datasets.py
+++ src/datasets.py
@@ -44,9 +44,43 @@ class UnpairedDataset(Dataset):
 
         return image, label, patient_side
 
-# TODO: Implement paired dataset
 
-# TODO: Implement data loader
+class PairedDataset(Dataset):
+    def __init__(self, df, transform=TRANSFORM):
+        df_pre = df[df['surgery'] == 'pre']
+        df_pre = df_pre.rename(columns={'path': 'path_pre'})
+        df_pre = df_pre.rename(columns={'filename': 'filename_pre'})
+        df_pre = df_pre.drop(columns='surgery')
+
+        df_post = df[df['surgery'] == 'post']
+        df_post = df_post.rename(columns={'path': 'path_post'})
+        df_post = df_post.rename(columns={'filename': 'filename_post'})
+        df_post = df_post.drop(columns='surgery')
+
+        self.df = pd.merge(left=df_pre, right=df_post)
+        self.df = self.df.reset_index()
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, index):
+        image_pre = plt.imread(self.df.loc[index, 'path_pre'])
+        image_post = plt.imread(self.df.loc[index, 'path_post'])
+        patient = self.df.loc[index, 'patient']
+        side = self.df.loc[index, 'side']
+        patient_side = patient + '_' + side
+        label_pre = np.array([1, 0])
+        label_post = np.array([0, 1])
+
+        if self.transform:
+            image_pre = self.transform(image=image_pre)['image']
+            image_post = self.transform(image=image_post)['image']
+
+        image_pre = image_pre.mean(axis=0, keepdims=True)
+        image_post = image_post.mean(axis=0, keepdims=True)
+
+        return image_pre, image_post, label_pre, label_post, patient_side
 
 
 def load_data(args):
@@ -54,11 +88,9 @@ def load_data(args):
 
 
 if __name__ == '__main__':
-    df = pd.read_csv('../data/data.csv')
-    df['path'] = df['path'].apply(lambda x: '../' + x)
+    df = pd.read_csv('data/data.csv')
     train_dataset = UnpairedDataset(df)
     print(len(train_dataset))
-
     sample = train_dataset[0]
     print(sample[0].shape)
     print(sample[1])
@@ -67,7 +99,30 @@ if __name__ == '__main__':
     num_workers = 4 * torch.cuda.device_count()
     train_loader = DataLoader(train_dataset, shuffle=True,
                               batch_size=8, num_workers=num_workers)
-    for i, (image, label, patient_side) in enumerate(train_loader):
-        print(image.shape)
-        print(label.shape)
-        print(patient_side)
+
+    image, label, patient_side = next(iter(train_loader))
+    print(image.shape)
+    print(label.shape)
+    print(patient_side)
+    print('------------------')
+
+    train_dataset = PairedDataset(df)
+    print(len(train_dataset))
+    sample = train_dataset[0]
+    print(sample[0].shape)
+    print(sample[1].shape)
+    print(sample[2].shape)
+    print(sample[3].shape)
+    print(sample[4])
+
+    num_workers = 4 * torch.cuda.device_count()
+    train_loader = DataLoader(train_dataset, shuffle=True,
+                              batch_size=8, num_workers=num_workers)
+
+    image_pre, image_post, label_pre, label_post, patient_side = next(iter(train_loader))
+    print(image_pre.shape)
+    print(image_post.shape)
+    print(label_pre.shape)
+    print(label_post.shape)
+    print(patient_side)
+    print('------------------')
--- src/model_trainers.py
+++ src/model_trainers.py
@@ -1,22 +1,21 @@
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 from src.losses import KL_div_N01
 
 
 def train_unpaired(model, train_loader, device, lr, epochs):
-    # define optimizer, loss function, and so on..
-    loss_fn_MSE, loss_fn_KL = nn.MSELoss(), KL_div_N01
+    loss_fn_MSE = nn.MSELoss()
+    loss_fn_KL = KL_div_N01()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
     for epoch in range(epochs):
-        print(f"\nRunning Epoch # {epoch}")
+        print(f"\nRunning Epoch #{epoch}")
 
-        running_total_loss = 0.
-        running_mse_loss = 0.
-        running_kl_loss = 0.
+        running_total_loss = 0.0
+        running_mse_loss = 0.0
+        running_kl_loss = 0.0
 
         for image, label, patient_side in tqdm(train_loader):
             image = image.to(device)
@@ -34,3 +33,50 @@ def train_unpaired(model, train_loader, device, lr, epochs):
             running_total_loss += loss.item()
             running_mse_loss += mse_loss.item()
             running_kl_loss += kl_loss.item()
+
+
+def train_paired(model, train_loader, device, lr, epochs):
+    loss_fn_MSE = nn.MSELoss()
+    loss_fn_KL = KL_div_N01()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    for epoch in range(epochs):
+        print(f"\nRunning Epoch #{epoch}")
+
+        running_total_loss = 0.0
+        running_mse_loss = 0.0
+        running_kl_loss = 0.0
+        running_synth_loss = 0.0
+
+        for image_pre, image_post, label_pre, label_post, patient_side in tqdm(train_loader):
+            image_pre = image_pre.to(device)
+            image_post = image_post.to(device)
+
+            # pre-op reconstruction
+            image_pre_recon, mean_pre, log_sigma_sq_pre = model.forward_train(image_pre, label_pre)
+            mse_loss_pre = loss_fn_MSE(image_pre_recon, image_pre)
+            kl_loss_pre = loss_fn_KL(mean_pre, log_sigma_sq_pre)
+
+            # post-op reconstruction
+            image_post_recon, mean_post, log_sigma_sq_post = model.forward_train(image_post, label_post)
+            mse_loss_post = loss_fn_MSE(image_post_recon, image_post)
+            kl_loss_post = loss_fn_KL(mean_post, log_sigma_sq_post)
+
+            # synthesis post-op
+            z_pre = model.encode_mean_logsigsq_to_z(mean_pre, log_sigma_sq_pre)
+            image_post_synth = model.decode_z_to_output(z_pre, label_post)
+            synth_loss = loss_fn_MSE(image_post_synth, image_post)
+
+            # add losses
+            loss = mse_loss_pre + kl_loss_pre + mse_loss_post + kl_loss_post + synth_loss
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            running_total_loss += loss.item()
+            running_mse_loss += mse_loss_pre.item()
+            running_kl_loss += kl_loss_pre.item()
+            running_mse_loss += mse_loss_post.item()
+            running_kl_loss += kl_loss_post.item()
+            running_synth_loss += synth_loss.item()

2f2f5546ad9cbeaf3d995f25049f90ff1f67df21
Gary Chung <kuan-i.chung@vanderbilt.edu>
2023-12-07 00:29:47 -0600

    refactor: move small componets to model_components

---
+++ src/model_components.py
@@ -0,0 +1,289 @@
+from functools import reduce
+
+import torch
+import torch.nn as nn
+
+
+class Encoder_CNN(nn.Module):
+    def __init__(self, input_size: (1, 256, 256),
+                 num_kernels=[16, 32, 64, 128, 256],
+                 n_z=26, n_fc_layers=5):
+        super(Encoder_CNN, self).__init__()
+
+        self.enc = nn.ModuleList()
+        for i in range(len(num_kernels)):
+            if i == 0:
+                self.enc.append(nn.Conv2d(input_size[0],
+                                          num_kernels[i], 3, 1, 1))
+            else:
+                self.enc.append(nn.Conv2d(num_kernels[i-1],
+                                          num_kernels[i], 3, 1, 1))
+            self.enc.append(nn.BatchNorm2d(num_kernels[i]))
+            self.enc.append(nn.ReLU())
+
+            self.enc.append(nn.Conv2d(num_kernels[i], num_kernels[i], 3, 1, 1))
+            self.enc.append(nn.BatchNorm2d(num_kernels[i]))
+            self.enc.append(nn.ReLU())
+
+            self.enc.append(nn.Conv2d(num_kernels[i], num_kernels[i], 3, 1, 1))
+            self.enc.append(nn.BatchNorm2d(num_kernels[i]))
+            self.enc.append(nn.ReLU())
+
+            self.enc.append(nn.MaxPool2d(2, 2))
+
+        # self.enc.append(nn.AdaptiveAvgPool2d((1, 1)))
+        self.enc.append(nn.Flatten())
+        linear_input_size = (
+            num_kernels[-1] *
+            (input_size[1]//(2**len(num_kernels))) *
+            (input_size[2]//(2**len(num_kernels)))
+        )
+        down_size_ratio = int((linear_input_size / n_z)**(1/(1+n_fc_layers)))
+
+        for i in range(n_fc_layers):
+            linear_output_size = linear_input_size//down_size_ratio
+            self.enc.append(nn.Linear(linear_input_size, linear_output_size))
+            self.enc.append(nn.BatchNorm1d(linear_output_size))
+            self.enc.append(nn.ReLU())
+            linear_input_size = linear_output_size
+
+        self.enc.append(nn.Linear(linear_input_size, 2 * n_z))
+        self.enc.append(nn.BatchNorm1d(2 * n_z))
+        self.enc.append(nn.ReLU())
+
+        self.enc = nn.Sequential(*self.enc)
+
+    def forward(self, x):
+        return self.enc(x.float())
+
+
+class Decoder_CNN(nn.Module):
+    def __init__(self, n_z=26, n_label=2,
+                 output_size=(1, 256, 256),
+                 num_kernels=[64, 32, 16, 8, 4],
+                 n_fc_layers=5):
+        super(Decoder_CNN, self).__init__()
+
+        self.dec = nn.ModuleList()
+        unflatten_shape = (
+            num_kernels[0],
+            output_size[1]//(2**len(num_kernels)),
+            output_size[2]//(2**len(num_kernels))
+        )
+        unflatten_shape_prod = reduce(lambda x, y: x * y, unflatten_shape)
+        linear_input_size = n_z + n_label
+        up_size_ratio = int((unflatten_shape_prod / linear_input_size) **
+                            (1 / (1 + n_fc_layers)))
+        print(f"Up size ratio: {up_size_ratio}")
+        for i in range(n_fc_layers):
+            linear_output_size = linear_input_size * up_size_ratio
+            self.dec.append(nn.Linear(linear_input_size, linear_output_size))
+            self.dec.append(nn.BatchNorm1d(linear_output_size))
+            self.dec.append(nn.ReLU())
+            linear_input_size = linear_output_size
+
+        self.dec.append(nn.Linear(linear_input_size, unflatten_shape_prod))
+        self.dec.append(nn.BatchNorm1d(unflatten_shape_prod))
+        self.dec.append(nn.ReLU())
+        self.dec.append(nn.Unflatten(1, unflatten_shape))
+
+        for i in range(len(num_kernels)-1):
+            self.dec.append(nn.Conv2d(num_kernels[i], num_kernels[i], 3, 1, 1))
+            self.dec.append(nn.BatchNorm2d(num_kernels[i]))
+            self.dec.append(nn.ReLU())
+
+            self.dec.append(nn.Conv2d(num_kernels[i], num_kernels[i], 3, 1, 1))
+            self.dec.append(nn.BatchNorm2d(num_kernels[i]))
+            self.dec.append(nn.ReLU())
+
+            self.dec.append(nn.ConvTranspose2d(num_kernels[i],
+                                               num_kernels[i+1], 3, 2, 1, 1))
+            self.dec.append(nn.BatchNorm2d(num_kernels[i+1]))
+            self.dec.append(nn.ReLU())
+
+        self.dec.append(nn.ConvTranspose2d(num_kernels[-1],
+                                           output_size[0], 3, 2, 1, 1))
+        self.dec.append(nn.BatchNorm2d(output_size[0]))
+        self.dec.append(nn.Sigmoid())
+        self.dec = nn.Sequential(*self.dec)
+
+    def forward(self, x):
+        return self.dec(x.float())
+
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels,
+                 kernel_size=3, padding=1) -> None:
+        super(DoubleConv, self).__init__()
+        self.conv = nn.ModuleList()
+        self.conv.append(nn.Conv2d(in_channels, out_channels,
+                                   kernel_size, padding=padding))
+        self.conv.append(nn.BatchNorm2d(out_channels))
+        self.conv.append(nn.ReLU(inplace=True))
+        self.conv.append(nn.Conv2d(out_channels, out_channels,
+                                   kernel_size, padding=padding))
+        self.conv.append(nn.BatchNorm2d(out_channels))
+        self.conv.append(nn.ReLU(inplace=True))
+        self.conv = nn.Sequential(*self.conv)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class DownBlock(nn.Module):
+    def __init__(self, in_channels, out_channels,
+                 kernel_size=3, padding=1) -> None:
+        super(DownBlock, self).__init__()
+        self.conv = nn.ModuleList()
+        self.conv.append(nn.MaxPool2d(2, 2))
+        self.conv.append(DoubleConv(in_channels, out_channels,
+                                    kernel_size, padding))
+        self.conv = nn.Sequential(*self.conv)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class UpBlock(nn.Module):
+    def __init__(self, in_channels, out_channels,
+                 kernel_size=3, padding=1) -> None:
+        super(UpBlock, self).__init__()
+        self.conv = nn.ModuleList()
+        self.conv.append(nn.Upsample(scale_factor=2, mode='bilinear',
+                                     align_corners=True))
+        self.conv.append(DoubleConv(in_channels, out_channels,
+                                    kernel_size, padding))
+        self.conv = nn.Sequential(*self.conv)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class Encoder_CVUnet(nn.Module):
+    def __init__(self, image_size: (1, 256, 256),
+                 num_enc_kernels=[16, 32, 64, 128, 256],
+                 n_z=26, n_fc_layers=5):
+        super(Encoder_CVUnet, self).__init__()
+
+        self.enc = nn.ModuleList()
+        self.enc.append(DoubleConv(image_size[0], num_enc_kernels[0]))
+        for i in range(len(num_enc_kernels)):
+            if i == 0:
+                self.enc.append(DownBlock(num_enc_kernels[i],
+                                          num_enc_kernels[i]))
+            else:
+                self.enc.append(DownBlock(num_enc_kernels[i-1],
+                                          num_enc_kernels[i]))
+
+        self.fc = nn.ModuleList()
+        self.fc.append(nn.Flatten())
+
+        linear_input_size = (
+            num_enc_kernels[-1] *
+            (image_size[1]//(2**len(num_enc_kernels))) *
+            (image_size[2]//(2**len(num_enc_kernels)))
+        )
+        down_size_ratio = int((linear_input_size/(2*n_z))**(1/(1+n_fc_layers)))
+
+        for i in range(n_fc_layers):
+            linear_output_size = linear_input_size // down_size_ratio
+            self.fc.append(nn.Linear(linear_input_size, linear_output_size))
+            self.fc.append(nn.BatchNorm1d(linear_output_size))
+            self.fc.append(nn.ReLU())
+            linear_input_size = linear_output_size
+
+        self.fc.append(nn.Linear(linear_input_size, 2 * n_z))
+        self.fc.append(nn.BatchNorm1d(2 * n_z))
+        self.fc.append(nn.ReLU())
+
+    def forward(self, x):
+        output = x
+        outputs = []
+        for layer in self.enc:
+            output = layer(output)
+            outputs.append(output)
+        for layer in self.fc:
+            output = layer(output)
+        return output, outputs
+
+
+class Decoder_CVUnet(nn.Module):
+    def __init__(self, n_z=26, n_label=2,
+                 image_size=(1, 256, 256),
+                 num_enc_kernels=[16, 32, 64, 128, 256],
+                 num_dec_kernels=[64, 32, 16, 8, 4],
+                 n_fc_layers=5):
+        super(Decoder_CVUnet, self).__init__()
+
+        assert len(num_enc_kernels) == len(num_dec_kernels), \
+            "Number of encoder and decoder kernels must be the same"\
+            f"got {len(num_enc_kernels)} and {len(num_dec_kernels)}"
+
+        self.fc = nn.ModuleList()
+        linear_input_size = n_z + n_label
+        unflatten_shape = (
+            num_dec_kernels[0],
+            image_size[1]//(2**len(num_dec_kernels)),
+            image_size[2]//(2**len(num_dec_kernels))
+        )
+        unflatten_shape_prod = reduce(lambda x, y: x * y, unflatten_shape)
+        up_size_ratio = int((unflatten_shape_prod / linear_input_size) **
+                            (1 / (1 + n_fc_layers)))
+        for i in range(n_fc_layers):
+            linear_output_size = linear_input_size * up_size_ratio
+            self.fc.append(nn.Linear(linear_input_size, linear_output_size))
+            self.fc.append(nn.BatchNorm1d(linear_output_size))
+            self.fc.append(nn.ReLU())
+            linear_input_size = linear_output_size
+
+        self.fc.append(nn.Linear(linear_input_size, unflatten_shape_prod))
+        self.fc.append(nn.BatchNorm1d(unflatten_shape_prod))
+        self.fc.append(nn.ReLU())
+        self.fc.append(nn.Unflatten(1, unflatten_shape))
+
+        self.dec = nn.ModuleList()
+        num_dec_kernels = [num_dec_kernels[0]] + num_dec_kernels
+        num_enc_kernels = [num_enc_kernels[0]] + num_enc_kernels
+        for i in range(len(num_dec_kernels)-1):
+            input_size = num_enc_kernels[-i-1] + num_dec_kernels[i]
+            output_size = num_dec_kernels[i+1]
+            if i == 0:
+                self.dec.append(DoubleConv(input_size, output_size))
+            else:
+                self.dec.append(UpBlock(input_size, output_size))
+        self.dec.append(DoubleConv(num_dec_kernels[-1], image_size[0]))
+
+        self.output_layers = nn.ModuleList()  # Fixed typo in variable name
+        self.output_layers.append(nn.Conv2d(image_size[0], image_size[0],
+                                           3, 1, 1))
+        self.output_layers.append(nn.BatchNorm2d(image_size[0]))
+        self.output_layers.append(nn.Sigmoid())
+
+    def forward(self, x, enc_outputs):
+        output = x
+        for layer in self.fc:
+            output = layer(output)
+        for i, layer in enumerate(self.dec):
+            output = torch.cat((output, enc_outputs[-i-1]), dim=1)
+            output = layer(output)
+        for layer in self.output_layers:
+            output = layer(output)
+        return output
+
+
+if __name__ == "__main__":
+    print("Testing Encoder_CVAE...")
+    x = torch.zeros((10, 1, 256, 256))
+    y = torch.zeros((10, 2))
+    NUM_ENC_KERNELS = [19, 41, 97]
+    NUM_DEC_KERNELS = [101, 53, 23]
+    encoder = Encoder_CVUnet(image_size=x.shape[1:], n_z=26,
+                             num_enc_kernels=NUM_ENC_KERNELS)
+    decoder = Decoder_CVUnet(image_size=x.shape[1:], n_z=26,
+                             num_enc_kernels=NUM_ENC_KERNELS,
+                             num_dec_kernels=NUM_DEC_KERNELS)
+
+    mu_logsigmasq, outputs = encoder.forward(x)
+    for output in outputs:
+        print(output.shape)
+    print(mu_logsigmasq.shape)
--- src/models.py
+++ src/models.py
@@ -1,9 +1,11 @@
-from functools import reduce
-
 import torch
 import torch.nn as nn
 
 from utils import convert_image_to_scalar, convert_scalar_to_image
+from model_components import (
+    Encoder_CNN, Decoder_CNN,
+    Encoder_CVUnet, Decoder_CVUnet
+)
 
 
 class CVAE_MLP(nn.Module):
@@ -80,112 +82,6 @@ class CVAE_MLP(nn.Module):
         return convert_scalar_to_image(output, image_shape), mean, log_sigma_sq
 
 
-class Encoder_CNN(nn.Module):
-    def __init__(self, input_size: (1, 256, 256),
-                 num_kernels=[16, 32, 64, 128, 256],
-                 n_z=26, n_fc_layers=5):
-        super(Encoder_CNN, self).__init__()
-
-        self.enc = nn.ModuleList()
-        for i in range(len(num_kernels)):
-            if i == 0:
-                self.enc.append(nn.Conv2d(input_size[0],
-                                          num_kernels[i], 3, 1, 1))
-            else:
-                self.enc.append(nn.Conv2d(num_kernels[i-1],
-                                          num_kernels[i], 3, 1, 1))
-            self.enc.append(nn.BatchNorm2d(num_kernels[i]))
-            self.enc.append(nn.ReLU())
-
-            self.enc.append(nn.Conv2d(num_kernels[i], num_kernels[i], 3, 1, 1))
-            self.enc.append(nn.BatchNorm2d(num_kernels[i]))
-            self.enc.append(nn.ReLU())
-
-            self.enc.append(nn.Conv2d(num_kernels[i], num_kernels[i], 3, 1, 1))
-            self.enc.append(nn.BatchNorm2d(num_kernels[i]))
-            self.enc.append(nn.ReLU())
-
-            self.enc.append(nn.MaxPool2d(2, 2))
-
-        # self.enc.append(nn.AdaptiveAvgPool2d((1, 1)))
-        self.enc.append(nn.Flatten())
-        linear_input_size = (
-            num_kernels[-1] *
-            (input_size[1]//(2**len(num_kernels))) *
-            (input_size[2]//(2**len(num_kernels)))
-        )
-        down_size_ratio = int((linear_input_size / n_z)**(1/(1+n_fc_layers)))
-
-        for i in range(n_fc_layers):
-            linear_output_size = linear_input_size//down_size_ratio
-            self.enc.append(nn.Linear(linear_input_size, linear_output_size))
-            self.enc.append(nn.BatchNorm1d(linear_output_size))
-            self.enc.append(nn.ReLU())
-            linear_input_size = linear_output_size
-
-        self.enc.append(nn.Linear(linear_input_size, 2 * n_z))
-        self.enc.append(nn.BatchNorm1d(2 * n_z))
-        self.enc.append(nn.ReLU())
-
-        self.enc = nn.Sequential(*self.enc)
-
-    def forward(self, x):
-        return self.enc(x.float())
-
-
-class Decoder_CNN(nn.Module):
-    def __init__(self, n_z=26, n_label=2,
-                 output_size=(1, 256, 256),
-                 num_kernels=[64, 32, 16, 8, 4],
-                 n_fc_layers=5):
-        super(Decoder_CNN, self).__init__()
-
-        self.dec = nn.ModuleList()
-        unflatten_shape = (
-            num_kernels[0],
-            output_size[1]//(2**len(num_kernels)),
-            output_size[2]//(2**len(num_kernels))
-        )
-        unflatten_shape_prod = reduce(lambda x, y: x * y, unflatten_shape)
-        linear_input_size = n_z + n_label
-        up_size_ratio = int((unflatten_shape_prod / linear_input_size)**(1/(1+n_fc_layers)))
-        print(f"Up size ratio: {up_size_ratio}")
-        for i in range(n_fc_layers):
-            linear_output_size = linear_input_size * up_size_ratio
-            self.dec.append(nn.Linear(linear_input_size, linear_output_size))
-            self.dec.append(nn.BatchNorm1d(linear_output_size))
-            self.dec.append(nn.ReLU())
-            linear_input_size = linear_output_size
-
-        self.dec.append(nn.Linear(linear_input_size, unflatten_shape_prod))
-        self.dec.append(nn.BatchNorm1d(unflatten_shape_prod))
-        self.dec.append(nn.ReLU())
-        self.dec.append(nn.Unflatten(1, unflatten_shape))
-
-        for i in range(len(num_kernels)-1):
-            self.dec.append(nn.Conv2d(num_kernels[i], num_kernels[i], 3, 1, 1))
-            self.dec.append(nn.BatchNorm2d(num_kernels[i]))
-            self.dec.append(nn.ReLU())
-
-            self.dec.append(nn.Conv2d(num_kernels[i], num_kernels[i], 3, 1, 1))
-            self.dec.append(nn.BatchNorm2d(num_kernels[i]))
-            self.dec.append(nn.ReLU())
-
-            self.dec.append(nn.ConvTranspose2d(num_kernels[i],
-                                               num_kernels[i+1], 3, 2, 1, 1))
-            self.dec.append(nn.BatchNorm2d(num_kernels[i+1]))
-            self.dec.append(nn.ReLU())
-
-        self.dec.append(nn.ConvTranspose2d(num_kernels[-1],
-                                           output_size[0], 3, 2, 1, 1))
-        self.dec.append(nn.BatchNorm2d(output_size[0]))
-        self.dec.append(nn.Sigmoid())
-        self.dec = nn.Sequential(*self.dec)
-
-    def forward(self, x):
-        return self.dec(x.float())
-
-
 class CVAE_CNN(nn.Module):
     def __init__(
         self,
@@ -220,8 +116,10 @@ class CVAE_CNN(nn.Module):
 
         self.n_z = n_z
         self.n_label = n_label
-        self.encoder = Encoder_CNN(image_shape, encoder_kernels, n_z, fc_layers)
-        self.decoder = Decoder_CNN(n_z, n_label, image_shape, decoder_kernels, fc_layers)
+        self.encoder = Encoder_CNN(image_shape, encoder_kernels,
+                                   n_z, fc_layers)
+        self.decoder = Decoder_CNN(n_z, n_label, image_shape,
+                                   decoder_kernels, fc_layers)
 
     def encode_x_to_mean_logsigsq(self, x):
         enc_output = self.encoder(x)
@@ -256,154 +154,74 @@ class CVAE_CNN(nn.Module):
         return output, mean, log_sigma_sq
 
 
-# class CVUnet(nn.Module):
-#     def __init__(self, image_shape=(1, 256, 256),
-#                  encoder_kernels=[16, 32, 64, 128, 256],
-#                  decoder_kernels=[64, 32, 16, 8, 4],
-#                  n_z=26, n_label=2) -> None:
-#         super(CVUnet, self).__init__()
+class CVUnet(nn.Module):
+    def __init__(self, image_shape=(1, 256, 256),
+                 encoder_kernels=[16, 32, 64, 128, 256],
+                 decoder_kernels=[64, 32, 16, 8, 4],
+                 n_z=26, n_label=2,
+                 fc_layers=5) -> None:
+        super(CVUnet, self).__init__()
 
-#         assert len(encoder_kernels) == len(decoder_kernels), \
-#             "encoder and decoder must have the same number of layers"\
-#             f"got {len(encoder_kernels)} and {len(decoder_kernels)}"
+        assert len(encoder_kernels) == len(decoder_kernels), \
+            "encoder and decoder must have the same number of layers"\
+            f"got {len(encoder_kernels)} and {len(decoder_kernels)}"
 
-#         assert image_shape[1] % (2**len(encoder_kernels)) == 0, \
-#             "image size must be divisible by 2**len(encoder_kernels)"\
-#             f"got {image_shape[1]} and {2**len(encoder_kernels)}"
+        assert image_shape[1] % (2**len(encoder_kernels)) == 0, \
+            "image size must be divisible by 2**len(encoder_kernels)"\
+            f"got {image_shape[1]} and {2**len(encoder_kernels)}"
 
-#         assert image_shape[2] % (2**len(encoder_kernels)) == 0, \
-#             "image size must be divisible by 2**len(encoder_kernels)"\
-#             f"got {image_shape[2]} and {2**len(encoder_kernels)}"
+        assert image_shape[2] % (2**len(encoder_kernels)) == 0, \
+            "image size must be divisible by 2**len(encoder_kernels)"\
+            f"got {image_shape[2]} and {2**len(encoder_kernels)}"
 
-#         assert image_shape[1] % (2**len(decoder_kernels)) == 0, \
-#             "image size must be divisible by 2**len(decoder_kernels)"\
-#             f"got {image_shape[1]} and {2**len(decoder_kernels)}"
+        assert image_shape[1] % (2**len(decoder_kernels)) == 0, \
+            "image size must be divisible by 2**len(decoder_kernels)"\
+            f"got {image_shape[1]} and {2**len(decoder_kernels)}"
 
-#         assert image_shape[2] % (2**len(decoder_kernels)) == 0, \
-#             "image size must be divisible by 2**len(decoder_kernels)"\
-#             f"got {image_shape[2]} and {2**len(decoder_kernels)}"
+        assert image_shape[2] % (2**len(decoder_kernels)) == 0, \
+            "image size must be divisible by 2**len(decoder_kernels)"\
+            f"got {image_shape[2]} and {2**len(decoder_kernels)}"
 
-#         assert n_z % 2 == 0, \
-#             "n_z must be divisible by 2"\
-#             f"got {n_z}"
+        self.n_z = n_z
+        self.n_label = n_label
+        self.encoder = Encoder_CVUnet(image_shape, encoder_kernels,
+                                      n_z, fc_layers)
+        self.decoder = Decoder_CVUnet(n_z, n_label, image_shape,
+                                      encoder_kernels, decoder_kernels,
+                                      fc_layers)
 
-#         self.n_z = n_z
-#         self.n_label = n_label
+    def encode_x_to_mean_logsigsq(self, x):
+        enc_output, down_outputs = self.encoder(x)
+        mean = enc_output[..., :self.n_z]
+        log_sigma_sq = enc_output[..., self.n_z:]
 
-#         self.encoder_list = nn.ModuleList()
-#         self.decoder_list = nn.ModuleList()
+        return mean, log_sigma_sq, down_outputs
 
-#         for i in range(len(encoder_kernels)):
-#             if i == 0:
-#                 self.encoder_list.append(
-#                     self._down_block(image_shape[0], encoder_kernels[i]))
-#             else:
-#                 self.encoder_list.append(
-#                     self._down_block(encoder_kernels[i-1], encoder_kernels[i]))
+    def encode_mean_logsigsq_to_z(self, mean, log_sigma_sq):
+        sigma = log_sigma_sq.exp().sqrt()
+        z = mean + torch.randn_like(mean) * sigma
+        return z
 
-#         for i in range(len(decoder_kernels)):
-#             if i == 0:
-#                 self.decoder_list.append(
-#                     self._up_block(n_z + n_label, decoder_kernels[i]))
-#             else:
-#                 self.decoder_list.append(
-#                     self._up_block(decoder_kernels[i-1], decoder_kernels[i]))
+    def decode_z_to_output(self, z, lab, down_outputs):
+        # concatenate with label
+        z_lab = torch.cat((z, lab), axis=-1)
 
-#         self.encoder = nn.Sequential(*self.encoder_list)
-#         self.decoder = nn.Sequential(*self.decoder_list)
+        # decode
+        output = self.decoder(z_lab, down_outputs)
 
-#     def _double_conv(self, in_channels, out_channels,
-#                      kernel_size=3, padding=1):
-#         block = nn.Sequential(
-#             nn.Conv2d(in_channels, out_channels,
-#                       kernel_size, padding=padding),
-#             nn.BatchNorm2d(out_channels),
-#             nn.ReLU(),
-#             nn.Conv2d(out_channels, out_channels,
-#                       kernel_size, padding=padding),
-#             nn.BatchNorm2d(out_channels),
-#             nn.ReLU()
-#         )
-#         return block
+        return output
 
-#     def _down_block(self, in_channels, out_channels,
-#                     kernel_size=3, padding=1):
-#         block = nn.Sequential(
-#             nn.MaxPool2d(2, 2),
-#             nn.Conv2d(in_channels, out_channels,
-#                       kernel_size, padding=padding),
-#             nn.BatchNorm2d(out_channels),
-#             nn.ReLU(),
-#             nn.Conv2d(out_channels, out_channels,
-#                       kernel_size, padding=padding),
-#             nn.BatchNorm2d(out_channels),
-#             nn.ReLU()
-#         )
-#         return block
+    def forward(self, x, lab):
+        mean, log_sigma_sq, down_outputs = self.encode_x_to_mean_logsigsq(x)
+        z = self.encode_mean_logsigsq_to_z(mean, log_sigma_sq)
+        output = self.decode_z_to_output(z, lab, down_outputs)
+        return output
 
-#     def _up_block(self, in_channels, out_channels, kernel_size=3, padding=1):
-#         block = nn.Sequential(
-#             nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
-#             nn.ConvTranspose2d(in_channels, out_channels,
-#                                kernel_size, padding=padding),
-#             nn.BatchNorm2d(out_channels),
-#             nn.ReLU(),
-#             nn.ConvTranspose2d(out_channels, out_channels,
-#                                kernel_size, padding=padding),
-#             nn.BatchNorm2d(out_channels),
-#             nn.ReLU(),
-#         )
-#         return block
-
-#     def encode_x_to_mean_logsigsq(self, x):
-#         # start with double conv
-#         down_outputs = []
-#         output = self._down_block(x)
-#         down_outputs.append(output)
-
-#         # down blocks and save outputs
-#         for i in range(len(self.encoder_list)):
-#             output = self.encoder_list[i](output)
-#             down_outputs.append(output)
-
-#         # flatten
-#         output = nn.Flatten()(output)
-#         # linear layer to get mean and log_sigma_sq
-#         output = nn.Linear(output.shape[-1], 2 * self.n_z)(output)
-
-#         mean = output[..., :self.n_z]
-#         log_sigma_sq = output[..., self.n_z:]
-
-#         return mean, log_sigma_sq, down_outputs
-
-#     def encode_mean_logsigsq_to_z(self, mean, log_sigma_sq):
-#         sigma = log_sigma_sq.exp().sqrt()
-#         z = mean + torch.randn_like(mean) * sigma
-#         return z
-
-#     def decode_z_to_output(self, z, lab, down_outputs):
-#         # concatenate with label
-#         z_lab = torch.cat((z, lab), axis=-1)
-
-#         # unflatten
-#         output = nn.Unflatten(1, down_outputs[-1].shape[1:])(z_lab)
-
-#         # up blocks
-#         for i in range(len(self.decoder_list)):
-#             output = self.decoder_list[i](output)
-#             output = torch.cat((output, down_outputs[-i-2]), axis=1)
-
-#         # double conv
-#         output = self._double_conv(output.shape[1], output.shape[1])(output)
-
-#         return output
-
-#     def forward(self, x):
-
-#         # flatten
-#         output = nn.Flatten()(output)
-#         # linear layer to get mean and log_sigma_sq
-#         output = nn.Linear(output.shape[-1], 2 * self.n_z)(output)
+    def forward_train(self, x, lab):
+        mean, log_sigma_sq, down_outputs = self.encode_x_to_mean_logsigsq(x)
+        z = self.encode_mean_logsigsq_to_z(mean, log_sigma_sq)
+        output = self.decode_z_to_output(z, lab, down_outputs)
+        return output, mean, log_sigma_sq
 
 
 def get_model(args):
@@ -424,35 +242,20 @@ if __name__ == "__main__":
 
     x = torch.zeros((10, 1, 256, 256))
     y = torch.zeros((10, 2))
-    # cvae = CVAE_CNN(
-    #     encoder_kernels=encoder_kernels,
-    #     decoder_kernels=[128, 64, 32, 16, 8],
-    # )
-    # print(cvae(x, y).shape)
-    # enc = Encoder_CNN(x.shape[1:], [7, 7, 7])
-    # z = enc(x)
-    # print(enc)
-    # print(z.shape)
-    # dec = Decoder_CNN()
-    # print(dec(z).shape)
 
-    # x = torch.zeros((10, 1, 256, 256))
-    # y = torch.zeros((10, 2))
-
-    # cvae = CVAE_MLP(256**2,256**2, 5, 256, n_label=2)
-    # cvae = CVAE_CNN(x.shape[1:],
-    #                 [16, 32, 64, 128, 256],
-    #                 [64, 32, 16, 8, 4],
-    #                 n_z=26, n_label=2)
-
-    cvae = CVAE_CNN(x.shape[1:])
+    # cvae = CVAE_CNN(x.shape[1:])
     # print(cvae)
-    print(f"encoded shape: {cvae.encoder(x).shape}")
-    mean, logsigsq = cvae.encode_x_to_mean_logsigsq(x)
-    print(f"mean shape: {mean.shape}; logsigsq shape: {logsigsq.shape}")
-    z = cvae.encode_mean_logsigsq_to_z(mean, logsigsq)
-    print(f"z shape: {z.shape}")
-    output = cvae.decode_z_to_output(z, y)
-    print(f"decoded shape: {output.shape}")
-    print(cvae(x, y).shape)
-    print(cvae)
+    # print(f"encoded shape: {cvae.encoder(x).shape}")
+    # mean, logsigsq = cvae.encode_x_to_mean_logsigsq(x)
+    # print(f"mean shape: {mean.shape}; logsigsq shape: {logsigsq.shape}")
+    # z = cvae.encode_mean_logsigsq_to_z(mean, logsigsq)
+    # print(f"z shape: {z.shape}")
+    # output = cvae.decode_z_to_output(z, y)
+    # print(f"decoded shape: {output.shape}")
+    # print(cvae(x, y).shape)
+    # print(cvae)
+    NUM_ENC_KERNELS = [19, 41, 97]
+    NUM_DEC_KERNELS = [101, 53, 23]
+    cvunet = CVUnet(x.shape[1:], NUM_ENC_KERNELS, NUM_DEC_KERNELS)
+    print(cvunet)
+    print(cvunet(x, y).shape)
